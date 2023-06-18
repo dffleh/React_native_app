d79d16588ceffd295b173df2306e3a202b227eb5
dffleh <dffleh1995@gmail.com>
2023-06-18 13:10:19 +0300

    start

--- App.jsx
+++ App.jsx
@@ -1,10 +1,11 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import "expo-dev-client";
+import StartPage from "./StartPages";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <StartPage />
       <StatusBar style="auto" />
     </View>
   );
